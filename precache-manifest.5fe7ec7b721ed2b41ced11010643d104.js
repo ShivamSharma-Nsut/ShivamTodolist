1a56a1c705392af0adca42855ad21cc432974973
Shivam Sharma <shivam15951@gmail.com>
2020-05-21 14:02:38 +0530

    Updates

---
+++ precache-manifest.5fe7ec7b721ed2b41ced11010643d104.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283c7e876df88715b6def2de159cfdd6",
+    "url": "/ShivamTodolist/index.html"
+  },
+  {
+    "revision": "147658958eaae99fafef",
+    "url": "/ShivamTodolist/static/css/main.dd1bfd0b.chunk.css"
+  },
+  {
+    "revision": "9a2057263d4987a6df3f",
+    "url": "/ShivamTodolist/static/js/2.12ad9925.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ShivamTodolist/static/js/2.12ad9925.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "147658958eaae99fafef",
+    "url": "/ShivamTodolist/static/js/main.8de9bebd.chunk.js"
+  },
+  {
+    "revision": "10d10b1f86612cfd4d9c",
+    "url": "/ShivamTodolist/static/js/runtime-main.2f87e8e1.js"
+  }
+]);
